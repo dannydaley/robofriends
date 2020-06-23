faa5801ae48b234f3afbe85e289c79e211040106
dannydaley <dannydaley@outlook.com>
2020-06-23 21:19:06 +0100

    Updates

---
+++ precache-manifest.9105a203a8b1b62895a3577d085d2dda.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ebd5935458ad97033cb2faec9573339",
+    "url": "/dannydaley/robofriends.git/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/dannydaley/robofriends.git/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "e884262a96fd678aed36",
+    "url": "/dannydaley/robofriends.git/static/css/main.8cadf703.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/dannydaley/robofriends.git/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dannydaley/robofriends.git/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e884262a96fd678aed36",
+    "url": "/dannydaley/robofriends.git/static/js/main.43c4ffda.chunk.js"
+  },
+  {
+    "revision": "786e5a1e20ea8b1262be",
+    "url": "/dannydaley/robofriends.git/static/js/runtime-main.f8651541.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/dannydaley/robofriends.git/static/media/SEGA.12a3db8b.woff"
+  }
+]);
